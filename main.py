2040e7b3b35d30b453c2810788ce6df2ee4c0f9c
jetcouper <ant7945@gmail.com>
2026-02-12 12:19:55 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from sys import argv
+import re
+
+def main():
+    chemin = argv[1]
+    encodage = argv[2]
+    #methode = argv[3]
+
+    f = open(chemin, encoding=encodage)
+    texte = f.read()
+    texte = re.findall(r'\w+' , texte)
+    f.close()
+
+    return 0
+
+
+if __name__ == '__main__':
+    quit(main())
