ab7792bee8b8475b98b16b2935212297ee48cb76
Vincent <vince_little_doggy@hotmail.com>
2026-02-25 16:21:59 -0500

    ajout de city-bloc et moindre carre

--- recherche.py
+++ recherche.py
@@ -6,13 +6,7 @@ class Recherche:
     def __init__(self,chemin,encodage,matrice,dictionnaire,mot,nb_synonymes,methode):
         self.text = self.creationTexte(chemin,encodage)
         self.resultat = {}
-        if methode == "0":
-            self.resultat = self.scalaire(matrice, dictionnaire,mot,nb_synonymes)
-        elif methode == "1":
-            pass
-        elif methode == "2":
-            pass
-
+        self.resultat = self.calcul(matrice, dictionnaire,mot,nb_synonymes,methode)
 
     def creationTexte(self, chemin,encodage):
         f = open(chemin, encoding=encodage)
@@ -22,7 +16,7 @@ class Recherche:
         f.close()
         return texte_lower
 
-    def scalaire(self,matrice, dictionnaire,mot,nb_synonymes):
+    def calcul(self,matrice, dictionnaire,mot,nb_synonymes,methode):
         list_mot_score = {}
         score = 0
         list_mot = list(dictionnaire.keys())
@@ -33,15 +27,28 @@ class Recherche:
             print("Ce mot n'est pas présent dans le texte.")
         
         matrice_mot = matrice[index_mot]
-        tuple_matrice_mot = tuple(matrice_mot)
-        #mot_recherche = dictionnaire.
+        
         for index, valeur in enumerate(matrice):
             if index != index_mot:
-                score = np.dot(matrice_mot,valeur)
+                if methode == "0":
+                    #calcule scalaire
+                    score = np.dot(matrice_mot,valeur)
+                elif methode == "1":
+                    #calcule moindre_carres
+                    score = np.sum(np.square(matrice_mot - valeur))
+                elif methode == "2":
+                    #calcule city_block
+                    score = np.sum(np.abs(matrice_mot - valeur))
+                
                 list_mot_score[list_mot[index]] = score
-                pass
-        
-        liste_trier = dict(sorted(list_mot_score.items(), key= lambda item: item[1],reverse=True)[:nb_mot])
+        if methode == "0":
+            #maximiser résultat
+            liste_trier = dict(sorted(list_mot_score.items(), key= lambda item: item[1],reverse=True)[:nb_mot])
+        else:
+            #minimiser le resultat
+            liste_trier = dict(sorted(list_mot_score.items(), key= lambda item: item[1])[:nb_mot])   
         return liste_trier
-        pass
-        
+    
+    
+    
+    
