14e9170b8fc4e044f8f6aab40756a37b42b12858
jetcouper <Yoshi9603!?Git>
2026-02-26 10:44:10 -0500

    Stop-word Terminer

--- main.py
+++ main.py
@@ -4,25 +4,25 @@ from entrainement import Entrainement
 from recherche import Recherche
 
 def main():
-    chemin = argv[1]
+    chemin = argv[3]
     encodage = argv[2]
-    #fenetre = argv[3]
-    fenetre = 5
-    entrainementText = Entrainement(chemin,encodage,fenetre)
+    fenetre = argv[1]
+    #fenetre = 5
+    entrainementText = Entrainement(chemin,encodage,int(fenetre))
 
     commande_complete = ""
     while commande_complete != "q":
         commande_complete = input("Entrez un mot, le nombre de synonymes que vous voulez et la méthode de calcule, i.e. produit scalaire: 0, least-squares:1, city-block: 2\r\nTapez q pour quitter.\r\n")
-        mot = commande_complete.split()[0]
-        nb_synonymes = commande_complete.split()[1]
-        methode = commande_complete.split()[2]
-        recherche = Recherche(chemin,encodage,entrainementText.matrice,entrainementText.dict,mot,nb_synonymes,methode)
-        print("\r")
-        for key, value in recherche.resultat.items():
-            nombre = value.item()
-            
-            print(f"{key} --> {nombre}")
-        print("\r")
+        if commande_complete != 'q':
+            mot = commande_complete.split()[0]
+            nb_synonymes = commande_complete.split()[1]
+            methode = commande_complete.split()[2]
+            recherche = Recherche(chemin,encodage,entrainementText.matrice,entrainementText.dict,mot,nb_synonymes,methode)
+            print("\r")
+            for key, value in recherche.resultat.items():
+                nombre = value.item()
+                print(f"{key} --> {nombre}")
+            print("\r")
 
         
 
--- recherche.py
+++ recherche.py
@@ -16,6 +16,13 @@ class Recherche:
         f.close()
         return texte_lower
 
+    def stopwords(self):
+        list_stop = []
+        stop_words = "où qu je tu il ils nous vous le la les un une des mon ton son nôtres notre vôtres votre leur leurs mes tes ces ses nos vos au aux avec ce dans de du elle en et eux lui ma mais me même moi ne on ou par pas pour qu' que qui sa se sur ta te toi ceci cela cet cette ici quel quels quelle quelles sans soi ainsi alors car comme donc lorsque puisque quoique si sinon tandis toutefois or ni voire aussi bien déjà encore jamais moins plus toujours très trop assez autant ailleurs alors après avant hier demain souvent parfois vite tard tôt ainsi presque aucun aucune certain certaine certains certaines chaque plusieurs quelconque tout toute tous toutes a b c d e f g h i j k l m n o p q r s t u v w x y z à â ä æ ç é è ê ë î ï ô ö œ ù û ü ÿ À Â Ä Æ Ç É È Ê Ë Î Ï Ô Ö Œ Ù Û Ü Ÿ"
+        list_stop = stop_words.split()
+        return list_stop
+
+
     def calcul(self,matrice, dictionnaire,mot,nb_synonymes,methode):
         list_mot_score = {}
         score = 0
@@ -41,6 +48,10 @@ class Recherche:
                     score = np.sum(np.abs(matrice_mot - valeur))
                 
                 list_mot_score[list_mot[index]] = score
+        liste_stopword = self.stopwords()
+        for i in liste_stopword:
+            if i in list_mot_score:
+                del list_mot_score[i]
         if methode == "0":
             #maximiser résultat
             liste_trier = dict(sorted(list_mot_score.items(), key= lambda item: item[1],reverse=True)[:nb_mot])
@@ -48,7 +59,3 @@ class Recherche:
             #minimiser le resultat
             liste_trier = dict(sorted(list_mot_score.items(), key= lambda item: item[1])[:nb_mot])   
         return liste_trier
-    
-    
-    
-    
