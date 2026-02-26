93941135cb2ef89eb2658b0aeff1e7338c17a44f
jetcouper <Yoshi9603!?Git>
2026-02-26 12:27:23 -0500

    Perfectionner le code

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from sys import argv, exit
-
 from entrainement import Entrainement
 from recherche import Recherche
+
+def boucle(entrainementText, chemin, encodage):
+    commande_complete = ""
+    while commande_complete != "q":
+        commande_complete = input("Entrez un mot, le nombre de synonymes que vous voulez et la méthode de calcule, i.e. produit scalaire: 0, least-squares:1, city-block: 2\r\nTapez q pour quitter.\r\n")
+        if commande_complete == 'q':
+            break
+        try:
+            mot, nb_synonymes, methode = commande_complete.split()
+            nb_synonymes = int(nb_synonymes)
+            methode = int(methode)
+            if methode not in (0, 1, 2):
+                print("La méthode doit être 0, 1 ou 2.\n")
+                continue
+        except ValueError:
+            print("Veuillez entrer : mot nombre methode\n")
+            continue
+
+        except IndexError:
+            print("Veuillez entrer exactement 3 valeurs.\n")
+            continue
+        recherche = Recherche(chemin,encodage,entrainementText.matrice,entrainementText.dict,mot,nb_synonymes,methode)
+        print("\r")
+        for key, value in recherche.resultat.items():
+            print(f"{key} --> {value}")
+        print("\r")
 
 def main():
     chemin = argv[3]
@@ -9,22 +34,7 @@ def main():
     fenetre = argv[1]
     #fenetre = 5
     entrainementText = Entrainement(chemin,encodage,int(fenetre))
-
-    commande_complete = ""
-    while commande_complete != "q":
-        commande_complete = input("Entrez un mot, le nombre de synonymes que vous voulez et la méthode de calcule, i.e. produit scalaire: 0, least-squares:1, city-block: 2\r\nTapez q pour quitter.\r\n")
-        if commande_complete != 'q':
-            mot = commande_complete.split()[0]
-            nb_synonymes = commande_complete.split()[1]
-            methode = commande_complete.split()[2]
-            recherche = Recherche(chemin,encodage,entrainementText.matrice,entrainementText.dict,mot,nb_synonymes,methode)
-            print("\r")
-            for key, value in recherche.resultat.items():
-                nombre = value.item()
-                print(f"{key} --> {nombre}")
-            print("\r")
-
-        
-
+    boucle(entrainementText, chemin, encodage)
+    
 if __name__ == '__main__':
     quit(main())
--- recherche.py
+++ recherche.py
@@ -18,7 +18,7 @@ class Recherche:
 
     def stopwords(self):
         list_stop = []
-        stop_words = "où qu je tu il ils nous vous le la les un une des mon ton son nôtres notre vôtres votre leur leurs mes tes ces ses nos vos au aux avec ce dans de du elle en et eux lui ma mais me même moi ne on ou par pas pour qu' que qui sa se sur ta te toi ceci cela cet cette ici quel quels quelle quelles sans soi ainsi alors car comme donc lorsque puisque quoique si sinon tandis toutefois or ni voire aussi bien déjà encore jamais moins plus toujours très trop assez autant ailleurs alors après avant hier demain souvent parfois vite tard tôt ainsi presque aucun aucune certain certaine certains certaines chaque plusieurs quelconque tout toute tous toutes a b c d e f g h i j k l m n o p q r s t u v w x y z à â ä æ ç é è ê ë î ï ô ö œ ù û ü ÿ À Â Ä Æ Ç É È Ê Ë Î Ï Ô Ö Œ Ù Û Ü Ÿ"
+        stop_words = "où qu je tu il ils nous vous le la les un une des mon ton son nôtres notre vôtres votre leur leurs mes tes ces ses nos vos au aux avec ce dans de du elle en et eux lui ma mais me même moi ne on ou par pas pour qu' que qui sa se sur ta te toi ceci cela cet cette ici quel quels quelle quelles sans soi ainsi alors car comme donc lorsque puisque quoique si sinon tandis toutefois or ni voire aussi bien déjà encore jamais moins toujours très trop assez autant ailleurs alors après avant hier demain souvent parfois vite tard tôt ainsi presque aucun aucune certain certaine certains certaines chaque plusieurs quelconque tout toute tous toutes a b c d e f g h i j k l m n o p q r s t u v w x y z à â ä æ ç é è ê ë î ï ô ö œ ù û ü ÿ À Â Ä Æ Ç É È Ê Ë Î Ï Ô Ö Œ Ù Û Ü Ÿ"
         list_stop = stop_words.split()
         return list_stop
 
@@ -37,13 +37,13 @@ class Recherche:
         
         for index, valeur in enumerate(matrice):
             if index != index_mot:
-                if methode == "0":
+                if methode == 0:
                     #calcule scalaire
                     score = np.dot(matrice_mot,valeur)
-                elif methode == "1":
+                elif methode == 1:
                     #calcule moindre_carres
                     score = np.sum(np.square(matrice_mot - valeur))
-                elif methode == "2":
+                elif methode == 2:
                     #calcule city_block
                     score = np.sum(np.abs(matrice_mot - valeur))
                 
@@ -52,7 +52,7 @@ class Recherche:
         for i in liste_stopword:
             if i in list_mot_score:
                 del list_mot_score[i]
-        if methode == "0":
+        if methode == 0:
             #maximiser résultat
             liste_trier = dict(sorted(list_mot_score.items(), key= lambda item: item[1],reverse=True)[:nb_mot])
         else:
