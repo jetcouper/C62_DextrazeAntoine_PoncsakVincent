8173a9958655f45ecaab71719987b3e678e5fff3
jetcouper <ant7945@gmail.com>
2026-04-10 09:08:31 -0400

    Petite correction

--- TP2/main.py
+++ TP2/main.py
@@ -29,19 +29,24 @@ def main():
             print("Mode prédiction activé")
             commande_complete = input("Entrez un mot, le nombre de synonymes que vous voulez et la méthode de calcule, i.e. produit scalaire: 0, least-squares:1, city-block: 2\r\nTapez q pour quitter.\r\n")
             while commande_complete != "q":
-                try:
-                    mot, nb_synonymes, methode = commande_complete.split()
-                    resultat = recherche.recherche(bd.charger_cooccurrences(argp.t), bd.charger_lexique(), mot, nb_synonymes, methode)
-                    print("\r")
-                    for key, value in resultat.items():
-                        print(f"{key} --> {value}")
-                    print("\r")
-                except ValueError:
-                    print("Veuillez entrer : mot nombre methode\n")
-                    continue
-                except Exception as e:
-                    print(e)
-                commande_complete = input("Entrez un mot, le nombre de synonymes que vous voulez et la méthode de calcule, i.e. produit scalaire: 0, least-squares:1, city-block: 2\r\nTapez q pour quitter.\r\n")
+                if commande_complete != "":
+                    try:
+                        mot, nb_synonymes, methode = commande_complete.split()
+                        resultat = recherche.recherche(bd.charger_cooccurrences(argp.t), bd.charger_lexique(), mot, nb_synonymes, methode)
+                        print("\r")
+                        for key, value in resultat.items():
+                            print(f"{key} --> {value}")
+                        print("\r")
+                    except ValueError:
+                        print("Veuillez entrer : mot nombre methode\n")
+                        continue
+                    except Exception as e:
+                        print(e)
+                    commande_complete = input("Entrez un mot, le nombre de synonymes que vous voulez et la méthode de calcule, i.e. produit scalaire: 0, least-squares:1, city-block: 2\r\nTapez q pour quitter.\r\n")
+                else:
+                    print("Veuillez entrer une commande valide, un mot, un nombre et une méthode.\n")
+                    commande_complete = input("Entrez un mot, le nombre de synonymes que vous voulez et la méthode de calcule, i.e. produit scalaire: 0, least-squares:1, city-block: 2\r\nTapez q pour quitter.\r\n")
+        
 
     
 if __name__ == '__main__':
