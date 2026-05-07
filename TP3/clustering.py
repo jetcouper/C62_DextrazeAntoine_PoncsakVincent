FIX: Chargement des matrices à partir de la DAO ADD: Ajout de la conservation et début de la normalisation

TP3/clustering.py
@@ -8,9 +8,10 @@ import statistics
 class Clustering():
     def __init__(self, nb_k, coocurance , lexique : dict[str, int]):
         self.__nb_k = nb_k
-        self.__coocurance = coocurance
         self.__lexique = lexique
-        self.matrice_mot = self.recreerMatriceMot()
+        self.matrice_mot = coocurance
+
+
 
 
         self.matrice_cluster = np.zeros(len(lexique), dtype=int)
@@ -20,7 +21,7 @@ class Clustering():
         print("Première assignation de cluster")
         t2 = perf_counter()
         self.assignerCluster()
-        print("Premier calcule de centoïde")
+        print("Premier calcule de centroïde")
         self.calculerCentroide()
         print(f"\r\nChargement des données en {(perf_counter() - t2):.2f} secondes")
         self.fit()
@@ -50,14 +51,6 @@ class Clustering():
         if np.array_equal(ancien_cluster,self.matrice_cluster):
             print(f"Arrêt après {compteur} itération")
     
-
-    def recreerMatriceMot(self) -> np.ndarray:
-        print("Recréation de matrice")
-        matrice_mot = np.zeros((len(self.__lexique), len(self.__lexique)))
-        for tuples in self.__coocurance:
-            mot1_id, mot2_id, compte = tuples
-            matrice_mot[mot1_id][mot2_id] = compte
-        return matrice_mot
 
     def obtenirMotProche(self,lexique_inverse)->defaultdict[list]:
         
@@ -99,11 +92,6 @@ class Clustering():
             for mot in mots[p][:nombre_retour]:
                 print(f"\t{mot[1]} -> {float(mot[0]):.2f}")
 
-
-
-
-        #for i in range(self.__nb_k):
-        #    mots[i] = np.where(self.matrice_cluster == i)[0]
 
                 
 

TP3/dao.py
@@ -1,4 +1,5 @@
 import sqlite3
+import numpy as np
 
 CHEMIN_BD = "cooccurrences.db"
 
@@ -77,6 +78,13 @@ class BaseDeDonnees:
         self.curseur.execute(SELECT_LEXIQUE)
         return {mot: id_ for mot, id_ in self.curseur.fetchall()}
 
-    def charger_cooccurrences(self, fenetre: int) -> list:
+    def charger_cooccurrences(self, fenetre: int) -> np.ndarray:
+        taille = len(self.charger_lexique())
         self.curseur.execute(SELECT_COOCCURRENCES, (fenetre,))
-        return self.curseur.fetchall()
+        matrice_mot = np.zeros((taille, taille))
+        for mot1_id, mot2_id, compte in self.curseur.fetchall():
+            matrice_mot[mot1_id][mot2_id] = compte
+        return matrice_mot
+
+
+        #return self.curseur.fetchall()

TP3/entrainement.py
@@ -4,13 +4,11 @@ from dao import BaseDeDonnees
 import re
 
 class Entrainement:
-    def __init__(self, chemin,encodage,fenetre, bd):
+    def __init__(self, fenetre, bd):
         self.bd = bd
         self._matrice = None
         self._dict = None
-        self.texte = self.creationTexte(chemin = chemin,encodage = encodage)
-        self._matrice, self._dict = self.creationMatrice(self.texte, fenetre,bd)
-        self.miseAJourBD(fenetre)
+        self.fenetre = fenetre
         
     @property
     def matrice(self):
@@ -28,15 +26,20 @@ class Entrainement:
     def dictionnaire(self,value):
         self._dict = value
 
-    def miseAJourBD(self, taille):
+    def entrainer(self,chemin,encodage):
+        texte = self.creationTexte(chemin,encodage)
+        self._matrice, self._dict = self.creationMatrice(texte)
+        self.miseAJourBD()
+
+    def miseAJourBD(self):
         self.bd.inserer_mots(self.dict.items())
         liste_tuple = []
         for i, j in np.argwhere(self.matrice > 0):
-            liste_tuple.append((int(i),int(j),taille,int(self.matrice[i,j])))
+            liste_tuple.append((int(i),int(j),self.fenetre,int(self.matrice[i,j])))
         self.bd.inserer_cooccurrences(liste_tuple)
     
 
-    def creationMatrice(self, texte,fenetre,bd):
+    def creationMatrice(self, texte):
         mot_a_index = self.bd.charger_lexique()
 
         for mot in texte:
@@ -46,7 +49,7 @@ class Entrainement:
         size = len(mot_a_index)
         # Create the 2D array of zeros with integer data type
         zero_matrix = np.zeros((size,size), dtype=int)
-        demi_fenetre = fenetre//2
+        demi_fenetre = self.fenetre//2
 
         for index, mot_central in enumerate(texte):
             i = mot_a_index[mot_central]
@@ -68,3 +71,22 @@ class Entrainement:
         texte = re.findall(r'\w+' , texte)
         f.close()
         return texte
+    
+    def chargerBD(self,normaliser=False, conserver = 0):
+        mot_a_index = self.bd.charger_lexique()
+        matrice_coocurance = self.bd.charger_cooccurrences(self.fenetre)
+        if conserver > 0:
+            sommes = np.sum(matrice_coocurance, axis=0)
+            indexes = np.argsort(sommes)[::-1][:conserver]
+            m = np.zeros((len(matrice_coocurance), conserver))
+            m[:] = matrice_coocurance[:, indexes][:]
+            matrice_coocurance = m
+        
+        if normaliser:
+            
+            ...
+        return mot_a_index, matrice_coocurance
+
+
+
+

TP3/recherche.py
@@ -7,10 +7,7 @@ class Recherche:
         pass
         
     def recherche(self, matrice,dictionnaire,mot,nb_synonymes,methode) -> dict:
-        matrice_refait = np.zeros((len(dictionnaire),len(dictionnaire)), dtype=int)
-        for tuples in matrice:
-            mot1_id, mot2_id, compte = tuples
-            matrice_refait[mot1_id][mot2_id] = compte
+        matrice_refait = matrice
         
 
         return self.calcul(matrice_refait, dictionnaire,mot,int(nb_synonymes),int(methode))